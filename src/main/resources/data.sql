5e55a59c90fc9c01e37aec450b6c9e1c457593b0
Nikita Zadiriushko <nzadiriushko@griddynamics.com>
2018-10-10 23:37:53 -0700

    more users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into customer (first_name, last_name, email) values
 ('Mark', 'Zuckerberg', 'zuk@facebook.com'),
-('Bill', 'Gates', 'bill@microsoft.com');
+('Bill', 'Gates', 'bill@microsoft.com'),
+('Jeff', 'Bezos', 'bezos@amazon.com'),
+('Elon', 'Musk',  'emusk@tesla.com');
